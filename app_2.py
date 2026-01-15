e7b7c9013df433af45dba402431a57773b33b516
alvescarolc33-cpu <alves.carol.c33@gmail.com>
2026-01-15 12:26:21 -0300

    ajuste

--- app_2.py
+++ app_2.py
@@ -23,12 +23,12 @@ def tela_login():
     if st.button("Entrar"):
         try:
             res = supabase.auth.sign_in_with_password({
-    "email": email,
-    "password": senha
-})
+            "email": email,
+            "password": senha
+            })
 
-st.session_state.user = res.user
-st.session_state.access_token = res.session.access_token
+            st.session_state.user = res.user
+            st.session_state.access_token = res.session.access_token
 
             st.success("Login realizado com sucesso!")
             st.rerun()
