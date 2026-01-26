42e735c4a945fbb93b38da700bc17498c67d8022
alvescarolc33-cpu <alves.carol.c33@gmail.com>
2026-01-26 18:43:08 -0300

    teste

--- app_2.py
+++ app_2.py
@@ -2,7 +2,7 @@ import os
 import io
 import pandas as pd
 import streamlit as st
-from supabase import create_client, Client
+from supabase import create_client, ClientOptions
 
 #-------INCLUSÃO DO CHAT
 if "user" not in st.session_state:
