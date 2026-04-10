655898e439d2c971a83b7f4b29b50c17142779b3
alvescarolc33-cpu <alves.carol.c33@gmail.com>
2026-04-10 15:44:58 -0300

    ajustes_diversos

--- pages/atos.py
+++ pages/atos.py
@@ -7,50 +7,51 @@ from services.supabase_client import get_supabase
 def pagina_atos():
 	st.title("⚖️ Atos TJRJ")
 
+    supabase = get_supabase()
 
 # ---- Formulário de Cadastro
 
-with st.form("cadastro_atos", clear_on_submit=True):
-    st.subheader("Novo Ato")
-    ato = st.text_input("Ato/Origem")
-    numero = st.number_input("Número")
-    ementa = st.text_input("Ementa")
-    data_doe = st.date_input("Data DOe")
-    folha_doe = st.number_input("Folha DOe")
-    link_doe = st.text_input("Link DOe")
-    comentarios = st.text_input("Comentário")
-    
-    submit_button = st.form_submit_button("Cadastrar Ato")
-
-
-# ---- Ação após submissão
-
-if submit_button:
-    if not numero:
-        st.error("O número do ato é obrigatório.")
-    else:
-        # Inserir no Supabase
-        response = supabase.table("atos_tj").insert({
-            "ato": ato,
-            "numero": numero,
-            "ementa": ementa,
-            "data_doe": data_doe,
-            "folha_doe": folha_doe,
-            "link_doe": link_doe,
-            "comentarios": comentarios,
-        }).execute()
+    with st.form("cadastro_atos", clear_on_submit=True):
+        st.subheader("Novo Ato")
+        ato = st.text_input("Ato/Origem")
+        numero = st.number_input("Número")
+        ementa = st.text_input("Ementa")
+        data_doe = st.date_input("Data DOe")
+        folha_doe = st.number_input("Folha DOe")
+        link_doe = st.text_input("Link DOe")
+        comentarios = st.text_input("Comentário")
         
-        if response.data:
-            st.success("Ato cadastrado com sucesso!")
+        submit_button = st.form_submit_button("Cadastrar Ato")
+
+
+    # ---- Ação após submissão
+
+    if submit_button:
+        if not numero:
+            st.error("O número do ato é obrigatório.")
         else:
-            st.error("Erro ao cadastrar.")
+            # Inserir no Supabase
+            response = supabase.table("atos_tj").insert({
+                "ato": ato,
+                "numero": numero,
+                "ementa": ementa,
+                "data_doe": data_doe.strftime("%Y-%m-%d"),
+                "folha_doe": folha_doe,
+                "link_doe": link_doe,
+                "comentarios": comentarios,
+            }).execute()
+            
+            if response.data:
+                st.success("Ato cadastrado com sucesso!")
+            else:
+                st.error("Erro ao cadastrar.")
 
 
-# ---- Visualização dos últimos processos
+    # ---- Visualização dos últimos processos
 
-st.divider()
-st.subheader("Atos Cadastrados")
-response = supabase.table("atos_tj").select("*").order("created_at", desc=True).limit(20).execute()
-if response.data:
-    df = pd.DataFrame(response.data)
-    st.dataframe(df[['ato', 'numero', 'ementa', 'data_doe, 'folha_doe, 'link_doe', 'comentarios', 'created_at']], use_container_width=True)
+    st.divider()
+    st.subheader("Atos Cadastrados")
+    response = supabase.table("atos_tj").select("*").order("created_at", desc=True).limit(20).execute()
+    if response.data:
+        df = pd.DataFrame(response.data)
+        st.dataframe(df[['ato', 'numero', 'ementa', 'data_doe', 'folha_doe', 'link_doe', 'comentarios', 'created_at']], use_container_width=True)
