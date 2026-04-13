0e3364a6b8c00539c3328b866d3d2e037004b15d
alvescarolc33-cpu <alves.carol.c33@gmail.com>
2026-04-13 14:08:35 -0300

    cor-fundo

--- app_2.py
+++ app_2.py
@@ -87,7 +87,7 @@ st.markdown(f"""
     left: 0;
     bottom: 0;
     width: 100%;
-    background-color: #F0F2F6;
+    background-color: #FCFCFC;
     color: #0E1117;
     text-align: center;
     padding: 8px;
