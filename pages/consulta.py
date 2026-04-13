f1a0c450a9416443fd178c8eda7ebdb85674bd04
alvescarolc33-cpu <alves.carol.c33@gmail.com>
2026-04-13 13:49:19 -0300

    ajuste_hiperlink

--- pages/consulta.py
+++ pages/consulta.py
@@ -270,6 +270,20 @@ def pagina_consulta():
             mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
 )
 
+st.divider()
+
+st.markdown(
+    """
+    <div style="font-size:0.9rem; text-align:center;">
+        🔗 <b>Fonte dos dados:</b><br>
+        <a href="https://transparencia.mprj.mp.br/web/novo-portal-transparencia/plantoes_novo" target="_blank">
+            Acessar página oficial
+        </a>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+
         # -------------------- Análises de Auxílios
 
         st.divider()
@@ -619,17 +633,3 @@ def pagina_consulta():
 
         else:
             st.info("Nenhuma ocorrência em outros órgãos para análise.")
-
-st.divider()
-
-st.markdown(
-    """
-    <div style="font-size:0.9rem; text-align:center;">
-        🔗 <b>Fonte dos dados:</b><br>
-        <a href="https://transparencia.mprj.mp.br/web/novo-portal-transparencia/plantoes_novo" target="_blank">
-            Acessar página oficial
-        </a>
-    </div>
-    """,
-    unsafe_allow_html=True
-)
