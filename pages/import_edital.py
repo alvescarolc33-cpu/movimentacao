e3949c0540fe60cdf63f8589f3b007cc026ef952
alvescarolc33-cpu <alves.carol.c33@gmail.com>
2026-09-25 11:06:28 -0300

    Refactor JSON validation and error handling in import
    Removed JSON validation for the main DataFrame and added error handling for individual records during the import process.

--- pages/import_edital.py
+++ pages/import_edital.py
@@ -149,14 +149,6 @@ def remover_duplicados(df):
     )
 
     return df[df["existe"] != True].drop(columns="existe")
-
-    dados = df.to_dict(orient="records")
-
-    try:
-        json.dumps(dados, allow_nan=False)
-        print("JSON válido")
-    except Exception as e:
-        print("Erro:", e)
         
 def importar(df):
 
@@ -178,7 +170,27 @@ def importar(df):
 
     df = df.where(pd.notnull(df), None)
     
+    #registros = df.to_dict("records")
     registros = df.to_dict("records")
+
+        for i, registro in enumerate(registros):
+            try:
+                json.dumps(registro, allow_nan=False)
+            except Exception as e:
+                st.write(f"REGISTRO COM ERRO: {i}")
+                st.write("ERRO:", e)
+        
+                for campo, valor in registro.items():
+                    try:
+                        json.dumps(valor, allow_nan=False)
+                    except Exception:
+                        st.write(
+                            f"CAMPO: {campo} | "
+                            f"TIPO: {type(valor)} | "
+                            f"VALOR: {repr(valor)}"
+                        )
+        
+                raise
 
     for registro in registros:
             for campo in ("codigo_titular", "ano"):
